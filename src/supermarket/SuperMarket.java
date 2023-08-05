cd2abc6ce3bcb4d1957a12a101ae63cc76b04b08
sachinthadilshan <sachinthadilshan96@gmail.com>
2023-08-05 19:11:02 +0530

    customer and inventory pages created

---
+++ src/supermarket/SuperMarket.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package supermarket;
+
+import supermarket.view.HomeView;
+import supermarket.view.InventoryView;
+import supermarket.view.UserView;
+
+/**
+ *
+ * @author sachinthadilshan
+ */
+public class SuperMarket {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new HomeView().setVisible(true);
+      
+    }
+    
+}
